331765af8d932f9d3d413d7a3b5771fe09db2c50
Sebastian Danielsson <sebastian.danielsson@protonmail.com>
2021-06-15 21:30:12 +0200

    Fix broken nav link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ module.exports = {
       items: [
         {
           type: 'doc',
-          docId: 'intro',
+          docId: 'introduction',
           position: 'left',
           label: 'Docs',
         },
