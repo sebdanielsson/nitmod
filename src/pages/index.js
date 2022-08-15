fe981ef5ef9213f92ceb74ccf0ab43d9d9696aed
sebastian <sebastian.danielsson@protonmail.com>
2022-08-15 11:49:44 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,38 +25,3 @@ function HomepageHeader() {
     </header>
   );
 }
-
-export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-
-  // Add Netlify Identity script
-  if (typeof window !== "undefined") {
-    // browser code
-    const document = window.document;
-    let script = document.createElement("script");
-    script.innerHTML = `
-      if (window.netlifyIdentity) {
-        window.netlifyIdentity.on("init", user => {
-          if (!user) {
-            window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-            });
-          }
-        });
-      }
-    `;
-  document.head.appendChild(script);
-  }
-
-  return (
-    <Layout>
-      <Head>
-         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-       </Head>
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
-}
