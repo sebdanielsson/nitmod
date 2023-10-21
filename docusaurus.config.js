d1869843e6251ba992e035a7f4d78879782f7889
Sebastian <sebastian.danielsson@proton.me>
2023-10-21 14:50:13 +0200

    Migrate to v3 (#119)
    * Migrate to v3
    
    * Remove search until support for v3 arrives

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  themes: [
+  /* themes: [
     [
       require.resolve("@easyops-cn/docusaurus-search-local"),
       {
@@ -20,129 +20,129 @@ module.exports = {
         explicitSearchResultPath: false
       },
     ],
-  ],
-  title: 'N!tmod',
-  tagline: 'Available for ET: Legacy 32-bit & 64-bit',
-  url: 'https://sebdanielsson.github.io',
-  baseUrl: '/nitmod/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/nitmod-square.webp',
-  organizationName: 'sebdanielsson', // Usually your GitHub org/user name.
-  projectName: 'nitmod', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  ], */
+  title: "N!tmod",
+  tagline: "Available for ET: Legacy 32-bit & 64-bit",
+  url: "https://sebdanielsson.github.io",
+  baseUrl: "/nitmod/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/nitmod-square.webp",
+  organizationName: "sebdanielsson", // Usually your GitHub org/user name.
+  projectName: "nitmod", // Usually your repo name.
+  deploymentBranch: "gh-pages",
   themeConfig: {
     colorMode: {
-      defaultMode: 'dark',
+      defaultMode: "dark",
       respectPrefersColorScheme: false,
     },
-    
+
     navbar: {
-      title: 'N!tmod',
+      title: "N!tmod",
       logo: {
-        alt: 'Nitmod Logo',
-        src: 'img/nitmod.webp',
+        alt: "Nitmod Logo",
+        src: "img/nitmod.webp",
       },
       items: [
         {
-          type: 'doc',
-          docId: 'installation',
-          position: 'left',
-          label: 'Docs',
+          type: "doc",
+          docId: "installation",
+          position: "left",
+          label: "Docs",
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        { to: "/blog", label: "Blog", position: "left" },
         {
-          href: 'http://stats.etmods.net',
-          label: 'Stats',
-          position: 'left',
-        },
-        {
-          href: 'http://bugs.etmods.net',
-          label: 'Bug Tracker',
-          position: 'left',
-        },
-        {to: '/servers', label: 'Servers', position: 'right'},
-        {
-          href: 'https://discord.gg/UHBpfFMHSX',
-          label: 'Discord',
-          position: 'right',
+          href: "http://stats.etmods.net",
+          label: "Stats",
+          position: "left",
         },
         {
-          href: 'https://forums.etmods.net',
-          label: 'Forums',
-          position: 'right',
+          href: "http://bugs.etmods.net",
+          label: "Bug Tracker",
+          position: "left",
+        },
+        { to: "/servers", label: "Servers", position: "right" },
+        {
+          href: "https://discord.gg/UHBpfFMHSX",
+          label: "Discord",
+          position: "right",
         },
         {
-          href: 'https://github.com/sebdanielsson/nitmod',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://forums.etmods.net",
+          label: "Forums",
+          position: "right",
         },
         {
-          href: 'https://www.paypal.com/donate?token=f_hY-YI0imkFarRsg7IouAp4SktL2zRMzD3_slsWKiwAX6zCsNXzX3lGLIV93zJRuO8Sn-1PQNh7KWgc',
-          label: 'Donate',
-          position: 'right',
+          href: "https://github.com/sebdanielsson/nitmod",
+          label: "GitHub",
+          position: "right",
+        },
+        {
+          href: "https://www.paypal.com/donate?token=f_hY-YI0imkFarRsg7IouAp4SktL2zRMzD3_slsWKiwAX6zCsNXzX3lGLIV93zJRuO8Sn-1PQNh7KWgc",
+          label: "Donate",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Installation',
-              to: '/docs/installation',
+              label: "Installation",
+              to: "/docs/installation",
             },
             {
-              label: 'Cvar reference',
-              to: '/docs/cvar-reference',
+              label: "Cvar reference",
+              to: "/docs/cvar-reference",
             },
             {
-              label: 'NxAC',
-              to: '/docs/nxac',
+              label: "NxAC",
+              to: "/docs/nxac",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Servers',
-              href: 'servers',
+              label: "Servers",
+              href: "servers",
             },
             {
-              label: 'Discord',
-              href: 'https://discord.gg/UHBpfFMHSX',
+              label: "Discord",
+              href: "https://discord.gg/UHBpfFMHSX",
             },
             {
-              label: 'Forums',
-              href: 'https://forums.etmods.net',
+              label: "Forums",
+              href: "https://forums.etmods.net",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/sebdanielsson/nitmod',
+              label: "GitHub",
+              href: "https://github.com/sebdanielsson/nitmod",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
+              label: "Blog",
+              to: "/blog",
             },
             {
-              label: 'Stats',
-              to: 'http://stats.etmods.net',
+              label: "Stats",
+              to: "http://stats.etmods.net",
             },
             {
-              label: 'Bug Tracker',
-              to: 'http://bugs.etmods.net',
+              label: "Bug Tracker",
+              to: "http://bugs.etmods.net",
             },
             {
-              label: 'Donate',
-              to: 'https://www.paypal.com/donate?token=f_hY-YI0imkFarRsg7IouAp4SktL2zRMzD3_slsWKiwAX6zCsNXzX3lGLIV93zJRuO8Sn-1PQNh7KWgc',
+              label: "Donate",
+              to: "https://www.paypal.com/donate?token=f_hY-YI0imkFarRsg7IouAp4SktL2zRMzD3_slsWKiwAX6zCsNXzX3lGLIV93zJRuO8Sn-1PQNh7KWgc",
             },
           ],
         },
@@ -152,22 +152,20 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/sebdanielsson/nitmod/edit/main/',
+          editUrl: "https://github.com/sebdanielsson/nitmod/edit/main/",
         },
         blog: {
           showReadingTime: false,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/sebdanielsson/nitmod/edit/main/blog/',
+          editUrl: "https://github.com/sebdanielsson/nitmod/edit/main/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
