93ac1bd3b14bf7d12ae2d144cb46d0845f922bc6
Sebastian <sebastian.danielsson@proton.me>
2025-08-10 10:38:33 +0200

    Use ts file extension for prettier config

---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from 'prettier';
+
+const config: Config = {
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: false,
+  trailingComma: 'all',
+  semi: true,
+  printWidth: 100,
+};
+
+export default config;
