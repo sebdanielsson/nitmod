59870008f937e424a40758aeb05bf7375166cc5c
Sebastian <sebastian.danielsson@proton.me>
2025-09-03 01:20:04 +0200

    Use defineConfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
-// @ts-check
-
+import { defineConfig } from "eslint/config";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 // import docusaurus from '@docusaurus/eslint-plugin'; // https://github.com/facebook/docusaurus/issues/10490
 
-export default [
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  // docusaurus.configs.recommended, // https://github.com/facebook/docusaurus/issues/10490
+export default defineConfig(
+  // Global ignores (applies to all configs below)
   {
-    files: ["*.ts", "*.tsx", "*.js", "*.mjs", "*.cjs"],
-    ignores: [],
+    ignores: ["build/**", "node_modules/**", ".docusaurus/**"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  //docusaurus.configs.recommended,
+  // Project-specific overrides (applies to all file types)
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
     rules: {},
   },
-];
+);
