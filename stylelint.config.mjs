c21707bbad615a0aa21fae8a69cbc6a5f8ad7e8a
Sebastian Danielsson <sebastian.danielsson@proton.me>
2026-01-17 22:12:48 +0100

    Switch to oxfmt

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ["stylelint-config-standard", "stylelint-config-html/html"],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-html/html",
+    "@dreamsicle.io/stylelint-config-tailwindcss",
+  ],
   rules: {
-    "at-rule-no-unknown": [
-      true,
-      {
-        ignoreAtRules: ["tailwind"],
-      },
-    ],
     "color-hex-length": null,
   },
 };
