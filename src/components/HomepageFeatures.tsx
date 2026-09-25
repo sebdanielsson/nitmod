ffb1a7f832c4f204714d2caa22421ae0adaf3062
Sebastian Danielsson <sebastian.danielsson@proton.me>
2026-09-25 20:27:19 +0200

    chore(deps): replace clsx with cn (#471)

--- src/components/HomepageFeatures.tsx
+++ src/components/HomepageFeatures.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import clsx from "clsx";
+import { clsx } from "cn/lite";
 import styles from "./HomepageFeatures.module.css";
 
 interface FeatureProps {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import clsx from "clsx";
+import { clsx } from "cn/lite";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
